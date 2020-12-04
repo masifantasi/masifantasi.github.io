5b4bc2959d46807b68523bb0e7daecde19808ce0
masifantasi <44269991+masifantasi@users.noreply.github.com>
2020-12-04 14:11:16 +0100

    Create maininventory.js

---
+++ art/myfactory/maininventory.js
@@ -0,0 +1,31 @@
+// baseeditmain.js
+
+(function() {
+
+  // dom ready
+  var isReady = document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll);
+
+  if (isReady) {
+    setTimeout(init, 0);
+  } else {
+    addEvent(window, 'load', function() {
+      setTimeout(init, 0);
+    });
+  }
+
+  // add event helper
+  function addEvent(obj, type, fn) {
+    if (obj.addEventListener) {
+      obj.addEventListener(type, fn);
+    } else if (obj.attachEvent) {
+      obj.attachEvent('on' + type, fn);
+    }
+  }
+
+  function init() {
+    alert('test');
+  }
+  
+  
+  
+  })();
